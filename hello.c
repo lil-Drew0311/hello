0ab1282dcc892423d33044ce99cb78050dd82e3c
Andrew Malesker <drewm2000@icloud.com>
2019-01-17 18:33:02 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Andrew Malesker
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Andrew!\n");
+
+  return 0;
+}
